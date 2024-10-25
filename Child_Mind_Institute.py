4a75bfcd4a323304be9b42fa02f1cce9dd466da8
GamingMike <Scoutgolddrummer@proton.me>
2024-10-25 16:56:57 -0400

    adding data

---
+++ Child_Mind_Institute.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print("Hello World")
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+
+
+
