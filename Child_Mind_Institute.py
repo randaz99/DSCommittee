e0c7d95653a2711927c5e47df2f16fc5ef276dfb
GamingMike <Scoutgolddrummer@proton.me>
2024-10-25 17:49:40 -0400

    Update

--- Child_Mind_Institute.py
+++ Child_Mind_Institute.py
@@ -3,9 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-print("Hello World")
+
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
+print(train.isna().sum())
+train = pd.get_dummies(train)
+train["Basic_Demos-Enroll_Season"] = [0 if i=="Spring"  i in train["Basic_Demos-Enroll_Seaso"]]
+train.dropna(inplace=False)
 
-
+sns.heatmap(train, cmap='coolwarm', annot=True)
