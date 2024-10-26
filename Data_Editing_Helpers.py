316a10056337c6b9ed0767a3f095fc447a31892d
randaz99 <randaz99@hotmail.com>
2024-10-25 20:08:08 -0400

    updating README.txt

--- Data_Editing_Helpers.py
+++ Data_Editing_Helpers.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pickle
+import os
+import pyarrow.parquet as pq
 
 
 def readData():
@@ -37,4 +40,26 @@ def makeSNS():
         plt.show()
     print("done")
 
-makeSNS()
+def unCacheOrLoad(file):
+    # Path for the cache file
+    name = file.replace(".csv",'')
+    name = name.replace(".parquet",'')
+    path = 'cache/' + name + '_cache.pkl'
+
+    # Check if the cache file exists
+    if os.path.exists(path):
+        # Load from cache
+        with open(path, 'rb') as file:
+            data = pickle.load(file)
+        print("read from cache")
+    else:
+        # Load from the original source
+        if file.__contains__(".csv"):
+            data = pd.read_csv(file)
+        else:
+            data = pq.read_table(file).to_pandas()
+        # Save to cache
+        with open(path, 'wb') as file:
+            pickle.dump(data, file)
+        print("Read from file")
+    return data
--- Main.py
+++ Main.py
@@ -1,5 +1,11 @@
 import Data_Editing_Helpers
 
-train, test = Data_Editing_Helpers.readData()
 
-Data_Editing_Helpers.makeSNS()
+#train, test = Data_Editing_Helpers.readData()
+
+#Data_Editing_Helpers.makeSNS()
+
+test = Data_Editing_Helpers.unCacheOrLoad("test.csv")
+train = Data_Editing_Helpers.unCacheOrLoad("train.csv")
+train_parq = Data_Editing_Helpers.unCacheOrLoad("series_train.parquet")
+#test_parq = Data_Editing_Helpers.unCacheOrLoad("series_test.parquet")
