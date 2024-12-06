93efe62f96f5e5d0b532b9e5aba63edf7b144e6a
randaz99 <randaz99@hotmail.com>
2024-12-06 13:32:23 -0500

    added comments

--- Main.py
+++ Main.py
@@ -1,26 +1,30 @@
 import Data_Editing_Helpers
 
-# Loading
+
+## Loading ##
 test = Data_Editing_Helpers.unCacheOrLoad("test.csv")
 train = Data_Editing_Helpers.unCacheOrLoad("train.csv")
 train_parq = Data_Editing_Helpers.unCacheOrLoad("series_train.parquet")
 test_parq = Data_Editing_Helpers.unCacheOrLoad("series_test.parquet")
 
-# Wrangling
+
+## Wrangling ##
 train, test = Data_Editing_Helpers.map_seasons(train, test)
 train, test = Data_Editing_Helpers.dropID(train, test, 'sii')
 train = Data_Editing_Helpers.remove_blank_rows(train)
 train, test = Data_Editing_Helpers.fill_NA(train, test, fill=99)
 
 
-# Visualizing
+## Visualizing ##
 #Data_Editing_Helpers.makeSNS(train)   # This oputputs all graphs, can be anoying
 #print(train)
 
-# Analyzing
+
+## Analyzing ##
 predictions = Data_Editing_Helpers.find_best_params(train, test, 'sii')
 
-# Submitting
+
+## Submitting ##
 Data_Editing_Helpers.generate_submission(predictions)
 
 
